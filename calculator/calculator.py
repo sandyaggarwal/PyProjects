e340cbc79f6120f85ea733e83884a4d009fd62bc
sandeep aggarwal <sandagga@adobe.com>
2025-04-26 18:52:39 +0530

    validate option value after input

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,8 +4,6 @@
 
 
 def calculate(first_number: int, second_number: int, choice: int) -> int:
-    if choice not in ([1, 2, 3, 4]):
-        raise ValueError('Enter valid option (1,2,3,4) only!')
     if choice == 1:
         return first_number + second_number
     if choice == 2:
@@ -16,6 +14,11 @@ def calculate(first_number: int, second_number: int, choice: int) -> int:
         return first_number / second_number
 
 
+def validate_option(operation_choice):
+    if operation_choice not in ([1, 2, 3, 4]):
+        raise ValueError('Enter valid option (1,2,3,4) only!')
+
+
 print('''
     Select operation:
     1. Add
@@ -24,6 +27,7 @@ print('''
     4. Division''')
 
 operation_choice = int(input('Enter Choice(1/2/3/4):'))
+validate_option(operation_choice)
 first_number = int(input('Enter firt number:'))
 second_number = int(input('Enter second number:'))
 answer = calculate(first_number, second_number, operation_choice)
