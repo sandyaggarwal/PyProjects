8522fcc4d21862aa5cfd24e3e15620891d9a490a
sandeep aggarwal <sandagga@adobe.com>
2025-05-02 14:10:03 +0530

    define setter and getter

--- LibMgmtSys/Book.py
+++ LibMgmtSys/Book.py
@@ -5,8 +5,13 @@ class Book:
         self.isbn: str = isbn
         self.__is_borrowed: bool = False
         
-        def set_is_borrowed(self, is_borrowed: bool):
-            self.__is_borrowed = is_borrowed
-        
-        def get_is_borrowed(self) -> bool:
-            return self.__is_borrowed
+    def set_is_borrowed(self, is_borrowed: bool):
+        self.__is_borrowed = is_borrowed
+    
+    def get_is_borrowed(self) -> bool:
+        return self.__is_borrowed
+
+       
+# famous_five: Book = Book('Famous Five', 'Unknown', '9234879237')
+# famous_five.set_is_borrowed(True)
+# print(f'Book {famous_five.title} status {famous_five.get_is_borrowed()}')   
