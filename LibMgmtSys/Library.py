ba3574d1b7568ef8715896ce4bc3c79dc5ac89e3
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 02:20:43 +0530

    improve implementation logic

--- LibMgmtSys/Library.py
+++ LibMgmtSys/Library.py
@@ -8,36 +8,38 @@ class Library:
         self.users: dict[str, User] = {}
 
     def add_book(self, book: Book):
-        if book.isbn in self.books.keys():
+        if book.isbn in self.books:
             print(f'Book {book.title} already available in library!')
             return
         self.books[book.isbn] = book
         print(f'Book: ({book.title}) -> added to library!')
 
     def remove_book(self, book: Book):
-        if book.isbn not in list(self.books.keys()):
+        if book.isbn not in self.books:
             print(f"Book with title {book.title} not available")
             return
-        self.books[book.isbn] = None
+        del self.books[book.isbn]
         print(f'Book {book.title} removed from library!')
 
     def issue_book(self, book: Book, user: User):
-        if book.isbn not in list(self.books.keys()):
+        if book.isbn not in self.books:
             print('Book not found in catalogue')
             return
-        if user.mobile_no not in list(self.users.keys()):
+        if user.mobile_no not in self.users:
             print('User not added to system, add user first!')
             return
-        if not book.get_is_borrowed():
-            book.set_is_borrowed(True)
-            book.set_borrowed_by(user.mobile_no)
-            print(f'Book: ({book.title}) issued to User {user.name}')
+        library_book = self.books[book.isbn]
+        if not library_book.get_is_borrowed():
+            library_book.set_is_borrowed(True)
+            library_book.set_borrowed_by(user.mobile_no)
+            print(f'Book: ({library_book.title}) issued to User {user.name}')
         else:
-            print(f'Book: ({book.title}) cant be issued')
+            print(f'Book: ({library_book.title}) cant be issued')
 
     def return_book(self, book: Book):
-        book.set_is_borrowed(False)
-        book.set_borrowed_by(None)
+        library_book = self.books[book.isbn]
+        library_book.set_is_borrowed(False)
+        library_book.set_borrowed_by(None)
 
     def list_books(self):
         if not self.books:
@@ -48,14 +50,14 @@ class Library:
             print(book)
 
     def add_user(self, user: User):
-        if user.mobile_no in list(self.users.keys()):
+        if user.mobile_no in self.users:
             print(f'User {user.name} with {user.mobile_no} already exist!')
             return
         self.users[user.mobile_no] = user
         print(f"User: {user.name} added to system!")
 
     def get_user_details(self, mobile_no: str) -> str:
-        if mobile_no not in list(self.users.keys()):
+        if mobile_no not in self.users:
             print("User details not found")
             return
         print(f'''User details:
