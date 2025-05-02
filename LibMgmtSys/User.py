5cf038f87c3e99b78a4f23fb9ecc048fff398484
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 00:07:23 +0530

    add email as optional property

--- LibMgmtSys/User.py
+++ LibMgmtSys/User.py
@@ -2,10 +2,11 @@ import uuid
 
 
 class User:
-    def __init__(self, name: str, mobile_no: str):
+    def __init__(self, name: str, mobile_no: str, email: str = None):
         self.__ID: str = uuid.uuid4()
         self.name: str = name
         self.mobile_no: str = mobile_no
-        
+        self.email: str = email | None
+
     def __str__(self):
         return f'User -> Name: {self.name}, MNo: {self.mobile_no}'
