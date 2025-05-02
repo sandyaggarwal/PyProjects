c7bdd69e7799b7ffd70b1d54681d334e36db1a14
sandeep aggarwal <sandagga@adobe.com>
2025-05-02 20:05:15 +0530

    print bookavailable status

--- LibMgmtSys/Book.py
+++ LibMgmtSys/Book.py
@@ -1,4 +1,5 @@
 class Book:
+
     def __init__(self, title, author, isbn):
         self.title: str = title
         self.author: str = author
@@ -13,6 +14,5 @@ class Book:
     
     def __str__(self):
         return (f'(Book(TItle:{self.title}, Author:{self.author},'
-                f'ISBN:{self.isbn}))')
-
-
+                f'ISBN:{self.isbn},' 
+                f'Borrowed: {"Yes" if self.__is_borrowed else "No"}')
