f324a4f108e89636f04730d138a443d8e806ae4d
sandeep aggarwal <sandagga@adobe.com>
2025-05-04 00:51:29 +0530

    fix indentation

--- RestroMenuSys/MenuItem.py
+++ RestroMenuSys/MenuItem.py
@@ -1,13 +1,13 @@
 class MenuItem:
-    def __init__(self, item_code: str, name: str, category: str):
+    def __init__(self, item_code: str, name: str, category: str = None):
         self.item_code: str = item_code
         self.name: str = name
         self.category: str = category
         self.__price: float = 0
 
-        def set_price(self, price: float):
-            self.__price = price
+    def set_price(self, price: float):
+        self.__price = price
 
-        def get_price(self):
-            return self.__price
+    def get_price(self):
+        return self.__price
 
