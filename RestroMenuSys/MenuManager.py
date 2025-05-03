8e46b7c8dffa0d0dfa09adf0a4906b0a6cc7d7d6
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 19:33:51 +0530

    create MenuManager class template

--- RestroMenuSys/MenuManager.py
+++ RestroMenuSys/MenuManager.py
@@ -1,2 +1,17 @@
 class MenuManager:
-    ...
+    def __init__(self):
+        pass
+
+    def add_menu_item():
+        ...
+
+    def remove_menu_item():
+        ...
+
+    def list_menu():
+        ...
+
+    def update_item_price():
+        ...
+
+
