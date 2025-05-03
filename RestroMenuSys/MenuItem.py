d25d89d13b0408b971ebf01de5b441d4d424e60c
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 20:42:04 +0530

    add category property

--- RestroMenuSys/MenuItem.py
+++ RestroMenuSys/MenuItem.py
@@ -1,7 +1,8 @@
 class MenuItem:
-    def __init__(self, item_code: str, name: str):
+    def __init__(self, item_code: str, name: str, category: str):
         self.item_code: str = item_code
         self.name: str = name
+        self.category: str = category
         self.__price: float = 0
 
         def set_price(self, price: float):
