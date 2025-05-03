dcb8aaf443907fd966e240b282da5ab238d051c1
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 20:03:02 +0530

    Implement add_menu_item

--- RestroMenuSys/MenuManager.py
+++ RestroMenuSys/MenuManager.py
@@ -1,9 +1,17 @@
+from MenuItem import MenuItem
+
+
 class MenuManager:
     def __init__(self):
-        pass
+        self.menu_items: dict[str, MenuItem] = {}
 
-    def add_menu_item():
-        ...
+    def add_menu_item(self, menu_item: MenuItem) -> bool:
+        if menu_item.item_code in self.menu_items:
+            print(f"\nItem: {menu_item.name} already avilable in Menu!!!\n")
+            return False
+        self.menu_items[menu_item.item_code] = menu_item
+        print(f"\nItem: {menu_item.name} added to Menu!!!\n")
+        return True
 
     def remove_menu_item():
         ...
