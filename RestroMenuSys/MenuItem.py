aa8755adf0be4d5372b00cd40fcf7381f88f2cef
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 19:49:27 +0530

    declare menu item properties

---
+++ RestroMenuSys/MenuItem.py
@@ -0,0 +1,12 @@
+class MenuItem:
+    def __init__(self, item_code: str, name: str):
+        self.item_code: str = item_code
+        self.name: str = name
+        self.__price: float = 0
+
+        def set_price(self, price: float):
+            self.__price = price
+
+        def get_price(self):
+            return self.__price
+
