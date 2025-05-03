c0f0f3f7f78c7864d75c320fe3d42c35d615a98f
sandeep aggarwal <sandagga@adobe.com>
2025-05-03 19:29:13 +0530

    Initial Restro Management System Push

---
+++ RestroMenuSys/Main.py
@@ -0,0 +1,10 @@
+from MenuManager import MenuManager
+
+
+class Main:
+    def __init__(self):
+        self.menu_manager: MenuManager = MenuManager()
+
+
+if __name__ == "__Main__":
+    main: Main = Main()
