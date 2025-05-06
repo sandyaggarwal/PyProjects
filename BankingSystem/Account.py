93f150f3f13176623d8f2c4dfd57c35d17fcffe2
sandeep aggarwal <san_eid@yahoo.co.in>
2025-05-06 23:50:12 +0530

    implement get balance

--- BankingSystem/Account.py
+++ BankingSystem/Account.py
@@ -6,7 +6,10 @@ class Account:
         self.account_number = random.randint(100000, 999999)
         self.account_name: str = account_name
         self.balance: float = initial_balance
-
+    
+    def get_balance(self):
+        return f"{self.balance:.2f}"
+        
     def __str__(self):
         return f"\n ********Account Details******** \nName: {self.account_name}\nAcctNumber:"\
                f"{self.account_number} \nBalance: {self.balance:.2f}"
