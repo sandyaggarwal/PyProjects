092ad77290f4dc2541b2dd560e3710d272b42800
sandeep aggarwal <san_eid@yahoo.co.in>
2025-05-07 00:39:12 +0530

    Implement withdraw feature

--- BankingSystem/Account.py
+++ BankingSystem/Account.py
@@ -1,5 +1,5 @@
 import random
-
+from InsufficientBalanceError import InsufficientBalanceError
 
 class Account:
     def __init__(self, account_name: str, initial_balance: float):
@@ -8,12 +8,29 @@ class Account:
         self.balance: float = initial_balance
  
     def get_balance(self):
+        print(f"Balance for the A/C {self.account_name} is {self.balance:.2f}")
         return f"{self.balance:.2f}"
 
     def deposit(self, amount: float) -> float:
         self.balance += amount
-        print(f"\n{amount} Rs credited to A/C {self.account_number}")
+        print(f"\nRs{amount} credited to A/C {self.account_number}")
         return self.get_balance()
+    
+    def is_valid_balance(self, amount: float):
+        if self.balance < amount:
+            raise InsufficientBalanceError
+        else:
+            return True
+        
+    def withdraw(self, amount: float):
+        try:
+            if self.is_valid_balance(amount):
+                self.balance -= amount
+                print(f"\n {amount} Rs amount withdrawn")
+                return self.get_balance()
+        except InsufficientBalanceError:
+            print(f"\nLow Balance!!! {self.get_balance()}"
+                  f"\nRs{amount} can't be withdrawn")
         
     def __str__(self):
         return f"\n ********Account Details******** \nName: {self.account_name}\nAcctNumber:"\
