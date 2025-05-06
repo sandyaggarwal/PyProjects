d9d739db2590dd2065317eb4ed381e4891d043d5
sandeep aggarwal <san_eid@yahoo.co.in>
2025-05-06 23:57:03 +0530

    Implement deposit feature

--- BankingSystem/Account.py
+++ BankingSystem/Account.py
@@ -6,9 +6,14 @@ class Account:
         self.account_number = random.randint(100000, 999999)
         self.account_name: str = account_name
         self.balance: float = initial_balance
-    
+ 
     def get_balance(self):
         return f"{self.balance:.2f}"
+
+    def deposit(self, amount: float) -> float:
+        self.balance += amount
+        print(f"\n{amount} Rs credited to A/C {self.account_number}")
+        return self.get_balance()
         
     def __str__(self):
         return f"\n ********Account Details******** \nName: {self.account_name}\nAcctNumber:"\
